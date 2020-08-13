7d5035664efbd3fd90bb2568a1186ac204f5bbb3
unknown <avanthi.divvela@gmail.com>
2020-08-13 11:26:50 -0500

    remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,5 @@
 //const e = require("express");
 
-console.log('Client-side java script file is loded');
-
 // fetch('http://puzzle.mead.io/puzzle').then((response) =>{
 //     response.json().then((data) => {
 //         console.log(data);
